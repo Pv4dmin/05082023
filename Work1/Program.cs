4ab0917b9f96f7d8ab25fc20dfa864d18c71e63e
Vyacheslav <pvadmin@mail.ru>
2023-08-11 23:33:40 +0800

    Квадрат числа

---
+++ Work1/Program.cs
@@ -0,0 +1,17 @@
+﻿// //Возведение в квадрат введенного числа
+// Console.Write("Введите число: ");
+// string num = Console.ReadLine()!;
+// int number = int.Parse(num);
+// int square = number * number;
+// Console.WriteLine("Квадрат числа " + number + " равен " + square); //либо
+// Console.WriteLine($"Квадрат числа {number} равен {square}");
+
+Console.Write("Введите число: ");
+// string num = Console.ReadLine()!; // "456"
+// int number = int.Parse(num); // 456
+
+int number = int.Parse(Console.ReadLine()!);
+
+int square = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + square);
+Console.WriteLine($"Квадрат числа {number} равен {square}");
