71e98d0cde91a576ab1b3a3c7b03420d8b0f7a19
Vyacheslav <pvadmin@mail.ru>
2023-08-11 23:45:19 +0800

    проверка квадрата первого от второго числа

---
+++ Work2/Program.cs
@@ -0,0 +1,14 @@
+﻿//напишите программу которая на вход приимаетдва числа и проверяет, является ли первое число квадратом второго
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number2 * number2 == number1)
+{
+Console.WriteLine($"Число {number1} являеется квадратом {number2}");
+}
+else
+{
+Console.WriteLine($"Число {number1} не являеется квадратом {number2}");
+}
