148c9ae25251768ca868a679737e07bf0ccb1a40
Vyacheslav <pvadmin@mail.ru>
2023-08-12 11:13:17 +0800

    честность числа

---
+++ HWork3/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа которая на вход принимает число и выдаёт, является ли оно четным или нет
+Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+int res = Num % 2;
+ if (res != 0)
+    {
+        Console.WriteLine("Число является нечетным");
+    }
+else 
+{
+    Console.WriteLine("Число является четным");
+}
