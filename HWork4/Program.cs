0667091e7683148154e1f85c2af020e892eaf26d
Vyacheslav <pvadmin@mail.ru>
2023-08-12 11:38:30 +0800

    вывод положительных чисел от 1 до n

---
+++ HWork4/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int start = 1;
+while (start < num)
+{
+    int number = start % 2;
+    if (number == 0)
+        {
+            Console.Write($"{start}; ");
+        }
+    start++;
+}
